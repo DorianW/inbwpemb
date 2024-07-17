fcc8a0982783b7be38b00afaf907a4424073c298
DorianW <dorianwojda@googlemail.com>
2024-07-17 13:40:18 +0200

    Add TextInput component

---
+++ src/Components/TextInput.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+type Props = {
+	onSubmit: (text: string) => void;
+}
+
+const TextInput = ({onSubmit}: Props) => {
+	const [text, setText] = useState<string>("");
+	const onClick = () => {
+		onSubmit(text);
+		setText('');
+	}
+	return (
+		<div>
+			<textarea value={text} onChange={(event) => setText(() => event.target.value)}></textarea>
+			<button onClick={onClick}>
+				Add comment
+			</button>
+		</div>
+	);
+}
+
+export default TextInput;
