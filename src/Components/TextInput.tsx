de992d56c5519b66a75bbaf1a699d573d74521a8
DorianW <dorianwojda@googlemail.com>
2024-07-17 17:33:55 +0200

    Add mobile view for TextInput

--- src/Components/TextInput.tsx
+++ src/Components/TextInput.tsx
@@ -11,13 +11,13 @@ const TextInput = ({onSubmit}: Props) => {
 		setText('');
 	}
 	return (
-		<form className="flex flex-col justify-start w-1/2">
+		<form className="flex flex-col justify-start w-full sm:w-6/12">
 			<textarea
 				className="p-4 rounded my-2"
 				value={text} onChange={(event) => setText(() => event.target.value)}
 				placeholder="Your comment..."
 			/>
-			<button className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-small rounded-lg text-sm px-4 py-2.5 me-2 mb-2 focus:outline-none w-1/3 ${!text ? 'cursor-not-allowed opacity-50' : ''}`}
+			<button className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-small rounded-lg text-sm px-4 py-2.5 me-2 mb-2 focus:outline-none w-full sm:w-1/2 ${!text ? 'cursor-not-allowed opacity-50' : ''}`}
 			        onClick={onClick}
 			        disabled={!text}
 			>
